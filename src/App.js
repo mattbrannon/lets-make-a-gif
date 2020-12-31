2e76769e75ef63a8bafd436ff6ad151b77f27707
ghostrib <sh4rkbot@gmail.com>
2020-12-31 08:55:17 -0600

    cConvert App to stateful component. Implement basic routing to child components.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
+import React, { Component } from 'react';
 import './App.css';
+import { Router } from '@reach/router';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="wrapper">
-        <p>I want to...</p>
-        <div className="choices">
-          <button className="first">Convert a video to a gif</button>
-          <button className="second">Convert images to a gif</button>
-        </div>
+import VideoConverter from './components/VideoConverter/VideoConverter';
+import ImageConverter from './components/ImageConverter/ImageConverter';
+import HomePage from './components/HomePage/HomePage';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoPage: false,
+      imagePage: false,
+    };
+  }
+
+  handleButtonClick = e => {
+    this.setState(prevState => {
+      return {
+        [e.target.name]: !prevState[e.target.name],
+      };
+    });
+  };
+
+  componentDidMount() {
+    this.setState({
+      videoPage: false,
+      imagePage: false,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          {!this.state.imagePage && !this.state.videoPage ? (
+            <HomePage path="/" handleButtonClick={this.handleButtonClick} />
+          ) : this.state.imagePage ? (
+            <ImageConverter path="image" />
+          ) : this.state.videoPage ? (
+            <VideoConverter path="video" />
+          ) : null}
+        </Router>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

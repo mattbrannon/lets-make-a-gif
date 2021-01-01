c8b36d675daa3f0ae3d70ee072f60069c4f1262a
ghostrib <sh4rkbot@gmail.com>
2021-01-01 02:22:19 -0600

    convert App to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { Router } from '@reach/router';
 
@@ -6,45 +6,16 @@ import VideoConverter from './components/VideoConverter/VideoConverter';
 import ImageConverter from './components/ImageConverter/ImageConverter';
 import HomePage from './components/HomePage/HomePage';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      videoPage: false,
-      imagePage: false,
-    };
-  }
-
-  handleButtonClick = e => {
-    this.setState(prevState => {
-      return {
-        [e.target.name]: !prevState[e.target.name],
-      };
-    });
-  };
-
-  componentDidMount() {
-    this.setState({
-      videoPage: false,
-      imagePage: false,
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          {!this.state.imagePage && !this.state.videoPage ? (
-            <HomePage path="/" handleButtonClick={this.handleButtonClick} />
-          ) : this.state.imagePage ? (
-            <ImageConverter path="image" />
-          ) : this.state.videoPage ? (
-            <VideoConverter path="video" />
-          ) : null}
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <HomePage path="/" />
+        <ImageConverter path="image" />
+        <VideoConverter path="video" />
+      </Router>
+    </div>
+  );
+};
 
 export default App;

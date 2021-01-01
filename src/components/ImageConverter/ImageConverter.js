c5a58b0a9027bc762f1023f0f3d1f54e6006bc3d
ghostrib <sh4rkbot@gmail.com>
2021-01-01 02:26:02 -0600

    render Header inside component

--- src/components/ImageConverter/ImageConverter.js
+++ src/components/ImageConverter/ImageConverter.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const ImageConverter = () => {
   return (
     <div>
+      <Header />
       <p>Image Converter</p>
     </div>
   );

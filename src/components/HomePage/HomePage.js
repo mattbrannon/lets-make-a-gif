50ac4dfca858d36b12ecaa5b98c11b0a43ecef33
ghostrib <sh4rkbot@gmail.com>
2021-01-01 02:29:14 -0600

    move NavButton component to HomePage folder

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from '@reach/router';
 
-import NavButton from '../NavButton/NavButton';
+import NavButton from './NavButton';
 
 const HomePage = () => {
   return (

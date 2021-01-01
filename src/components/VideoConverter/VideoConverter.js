2d2d33213aee88145ddb95e039e616fc9caaa99e
ghostrib <sh4rkbot@gmail.com>
2021-01-01 02:49:54 -0600

    add placeholder text for instructions

--- src/components/VideoConverter/VideoConverter.js
+++ src/components/VideoConverter/VideoConverter.js
@@ -6,6 +6,14 @@ const VideoConverter = () => {
     <div>
       <Header />
       <p>Video Converter</p>
+      <hr />
+      <h3>Instructions:</h3>
+      <p className="instructions">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem
+        expedita nemo a? Odio, tempora commodi dicta voluptates necessitatibus
+        magnam nulla eveniet cupiditate sed repudiandae incidunt asperiores ex
+        unde omnis culpa.
+      </p>
     </div>
   );
 };

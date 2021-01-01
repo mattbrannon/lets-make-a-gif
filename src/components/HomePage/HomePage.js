a17a38dc55a847511d8de4c52395e976eb9f4ef4
ghostrib <sh4rkbot@gmail.com>
2021-01-01 02:27:17 -0600

    refactor routing logic

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import { Link } from '@reach/router';
 
-const HomePage = ({ handleButtonClick }) => {
+import NavButton from '../NavButton/NavButton';
+
+const HomePage = () => {
   return (
     <div className="wrapper">
       <p>I want to...</p>
       <div className="choices">
         <Link to="video">
-          <button
-            name="videoPage"
-            onClick={handleButtonClick}
-            className="first"
-          >
-            Convert a video to a gif
-          </button>
+          <NavButton name="videoPage" text="Convert a video to a gif" />
         </Link>
         <Link to="image">
-          <button
-            name="imagePage"
-            onClick={handleButtonClick}
-            className="second"
-          >
-            Convert images to a gif
-          </button>
+          <NavButton name="imagePage" text="Convert images to a gif" />
         </Link>
       </div>
     </div>

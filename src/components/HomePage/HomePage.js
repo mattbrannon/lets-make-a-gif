05dd129008ecb8f24989ffef03766b314260ee3d
ghostrib <sh4rkbot@gmail.com>
2021-01-01 02:48:47 -0600

    style intro paragraph

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from '@reach/router';
 
 import NavButton from './NavButton';
+import s from './style.module.css';
 
 const HomePage = () => {
   return (
     <div className="wrapper">
-      <p>I want to...</p>
+      <p className={s.text}>I want to...</p>
       <div className="choices">
         <Link to="video">
           <NavButton name="videoPage" text="Convert a video to a gif" />

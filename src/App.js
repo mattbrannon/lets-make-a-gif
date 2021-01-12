157066221bbb166cef29806a147f6caadbb3411e
ghostrib <sh4rkbot@gmail.com>
2021-01-12 10:53:49 -0600

    add 404 page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Router } from '@reach/router';
 import VideoConverter from './components/VideoConverter/ConvertVideo';
 import ImageConverter from './components/ImageConverter/ConvertImages';
 import HomePage from './components/HomePage/HomePage';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <HomePage path="/" />
         <ImageConverter path="image" />
         <VideoConverter path="video" />
+        <NotFoundPage default />
       </Router>
     </div>
   );

2ed3749bff76f01656fd5a7e6f0cb5a67b3259f0
ghostrib <sh4rkbot@gmail.com>
2021-05-01 18:26:19 -0500

    this is becoming tedious

--- server/scripts/convert_images.sh
+++ server/scripts/convert_images.sh
@@ -4,12 +4,13 @@
 n=1
 dir="${PWD}"/images
 for f in "${dir}"/*; do
+  echo "$f"
   ext="${f#*.}"
   new=$(printf "image-%04d.${ext}" "$n")
   mv -- "$f" "${dir}/$new"
   ((n++))
 done
 
-ffmpeg -f image2 -framerate 12 -i "${dir}"/image-%04d.png "${PWD}"/uploads/new-file.gif
+ffmpeg -y -f image2 -framerate 12 -i "${dir}"/image-%04d.png "${PWD}"/uploads/new-file.gif
 
 rm "${dir}"/*
--- server/scripts/convert_video.sh
+++ server/scripts/convert_video.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-ffmpeg -loglevel quiet -i "$1" -vf "fps=12,scale=0:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "$PWD"/uploads/new-file.gif
+ffmpeg -y -loglevel quiet -i "$1" -vf "fps=12,scale=0:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "$PWD"/uploads/new-file.gif

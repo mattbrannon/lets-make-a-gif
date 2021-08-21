6b5c70c3f8b4f4ae94864106d779e229e9f1266a
mattbrannon <sh4rkbot@gmail.com>
2021-08-20 20:18:41 -0500

    update pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       script: 'server/index.js',
       watch: true,
-      ignore_watch: ['node_modules'],
+      ignore_watch: [ 'node_modules' ],
       env: {
         NODE_ENV: 'development',
       },
@@ -17,13 +17,23 @@ module.exports = {
     development: {
       user: 'gizmo',
       host: '161.35.131.122',
-      ref: 'origin/master',
-      repo: 'git@github.com:ghostrib/lets-make-a-gif.git',
+      ref: 'origin/dev',
+      repo: 'git@github.com:mattbrannon/lets-make-a-gif.git',
       path: '/var/www/lets-make-a-gif.com/',
       ssh_options: 'IdentityFile=~/.ssh/mattbrannon',
       'pre-deploy-local': '',
+      'post-deploy': 'yarn build && pm2 reload server/index.js --watch',
+    },
+    production: {
+      user: 'gizmo',
+      host: '161.35.131.122',
+      ref: 'origin/dev',
+      repo: 'git@github.com:mattbrannon/lets-make-a-gif.git',
+      path: '/var/www/lets-make-a-gif.com/',
+      ssh_options: 'IdentityFile=~/.ssh/mattbrannon',
+      // 'post-deploy': 'yarn build && pm2 reload server/index.js --watch',
       'post-deploy':
-        'npm run build && pm2 reload server/index.js --watch',
+        'yarn install && yarn build && pm2 reload ecosystem.config.js --env production && pm2 save',
     },
   },
 };

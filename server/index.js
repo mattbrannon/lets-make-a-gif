6d122a4537850c65a135744bf51464256d95d5e2
mattbrannon <sh4rkbot@gmail.com>
2021-08-20 19:08:44 -0500

    update server

--- server/index.js
+++ server/index.js
@@ -2,15 +2,21 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, '../build')));
 
 require('./routes/upload-images')(app);
-require('./routes/upload-video')(app);
+require('./routes/upload-image')(app);
+require('./routes/upload-videos')(app);
+require('./routes/upload-filters')(app);
 require('./routes/download')(app);
 require('./routes/default')(app);
 
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
